08997f7990ce07e4eccede8e916a51966df59066
drivenloop <90504853+drivenloop@users.noreply.github.com>
2023-01-23 23:18:06 +0300

    Update environment.ts

--- angularbutt/src/environments/environment.ts
+++ angularbutt/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-
+  firebase: {
+    projectId: 'anketprogram-6cd4d',
+    appId: '1:122486346483:web:91cd2c5f53afea51f79d86',
+    storageBucket: 'anketprogram-6cd4d.appspot.com',
+    apiKey: 'AIzaSyCz-arxVXl9CNyPP0weN-87qwRDruj_Cso',
+    authDomain: 'anketprogram-6cd4d.firebaseapp.com',
+    messagingSenderId: '122486346483',
+    measurementId: 'G-WYFENBE4CC',
+  },
 
 };
 
