0a428ad242122b33087a6643a4b562cd10526d7c
drivenloop <90504853+drivenloop@users.noreply.github.com>
2023-01-23 23:23:00 +0300

    Add files via upload

---
+++ köşeli popo/kaynak/ortamlar/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'anketprogram-6cd4d',
+    appId: '1:122486346483:web:91cd2c5f53afea51f79d86',
+    storageBucket: 'anketprogram-6cd4d.appspot.com',
+    apiKey: 'AIzaSyCz-arxVXl9CNyPP0weN-87qwRDruj_Cso',
+    authDomain: 'anketprogram-6cd4d.firebaseapp.com',
+    messagingSenderId: '122486346483',
+    measurementId: 'G-WYFENBE4CC',
+  },
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
